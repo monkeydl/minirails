8cc5420791f885688d0a9a2129da1ca949872d03
monkeydl <hung.pham@employmenthero.com>
2019-10-24 14:59:28 +0700

    Create a simple application that takes in a http request of type text and return status 200

--- lib/minirails.rb
+++ lib/minirails.rb
@@ -3,4 +3,14 @@ require "minirails/version"
 module Minirails
   class Error < StandardError; end
   # Your code goes here...
+
+  class Application
+    def call(_env)
+      [
+        200,
+        {'Content-Type' => 'text/html'},
+        ['Hello from Ruby on MiniRails!']
+      ]
+    end
+  end
 end
